3390b855f0c3827d8f0b809a51013ccec9a9772e
Winnie <wimacoj@gmail.com>
2024-01-15 11:10:26 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if n % 2 == 0]
+    if even_numbers == 0:
+        return even_numbers()
+    else :
+        return even_numbers
 
 def make_exclamation(sentence_list):
-    pass
+    exclamated = [sentence + "!" for sentence in sentence_list]
+    if exclamated == 0:
+        return exclamated()
+    else:
+        return exclamated
